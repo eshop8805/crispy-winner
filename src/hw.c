2925daff3516dba64a7775b239ca511448418ad5
Bill Elliott <eshop8805@verizon.net>
2020-03-17 22:28:38 -0500

    X Y Z loop

--- src/hw.c
+++ src/hw.c
@@ -7,4 +7,6 @@ int main() {
   printf("Combining (*dogs*) and (CATS) in master branch\n");
 
   for (int ii=0; ii < 4; ii++) printf("%c\n", 'A'+ii);
+  for (int jj=2; jj >= 0; jj--) printf("%c ", 'Z'-jj);
+  printf("\n");
 }
