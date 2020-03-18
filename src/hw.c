a429c3a77f83dd2c6a07daf8df60e37275c579d8
Bill Elliott <eshop8805@verizon.net>
2020-03-17 22:30:42 -0500

    Fixed bug 001

--- src/hw.c
+++ src/hw.c
@@ -7,6 +7,8 @@ int main() {
   printf("Combining (*dogs*) and (CATS) in master branch\n");
 
   for (int ii=0; ii < 4; ii++) printf("%c\n", 'A'+ii);
+  for (int kk=0; kk < 8; kk++) printf("%d ", kk);
   for (int jj=2; jj >= 0; jj--) printf("%c ", 'Z'-jj);
   printf("\n");
+  printf("BUG SHOULD BE FIXED\n");
 }
