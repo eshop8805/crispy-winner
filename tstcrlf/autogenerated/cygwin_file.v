8f158baa307c57f6a73a3d63536568bae9ef8664
EC2 Default User <ec2-user@ip-172-31-46-66.us-east-2.compute.internal>
2020-04-01 15:38:47 +0000

    Revert "2nd time saving files before renormalizing" Did not want to delete this files !!! This reverts commit aaddf08c320863736ad9adaa8c3299f29b941696.

---
+++ tstcrlf/autogenerated/cygwin_file.v
@@ -0,0 +1,9 @@
+This is a verliog file
+blah
+Addign a line from cygwin
+blah
+Blah should be here from cygwin
+This is random form linux
+new linux
+new cygwin
+blah
---
+++ tstcrlf/autogenerated/linux_file.h
@@ -0,0 +1,3 @@
+A linux file
+blah-dy EDITS IN cygwin
+blah
